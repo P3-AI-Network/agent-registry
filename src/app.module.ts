5b138455f588a90a1c035202dea7987c528e4c2c
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-03-13 15:50:31 +0530

    Minor/Misc Changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { AgentsModule } from './agents/agents.module';
@@ -18,6 +17,6 @@ import { SearchModule } from './search/search.module';
     SearchModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
